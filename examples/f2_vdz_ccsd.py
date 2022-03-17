b5f798e4913ab171820726dfd5d8339e2e5e9179
Karthik Gururangan <gururang@msu.edu>
2022-03-17 14:29:11 -0400

    Added a prototype of Stephen's experimental iterative CR-CC(2,3) method.
    Former-commit-id: ca746e3867e22aaf7195ad4ebc6b364caf5b1258

--- examples/f2_vdz_ccsd.py
+++ examples/f2_vdz_ccsd.py
@@ -28,23 +28,24 @@ system.print_info()
 
 calculation = Calculation(
     order=3,
-    calculation_type="ccsdt",
-    convergence_tolerance=1.0e-08
+    calculation_type="icrcc23",
+    convergence_tolerance=1.0e-08,
+    diis_size=6,
 )
 
 T, total_energy, is_converged = cc_driver(calculation, system, H)
 
-#calculation = Calculation(
+# calculation = Calculation(
 #    order=2,
 #    calculation_type="left_ccsd",
 #    convergence_tolerance=1.0e-08
-#)
-
-#Hbar = build_hbar_ccsd(T, H)
-
-#L, total_energy, is_converged = lcc_driver(calculation, system, T, Hbar, omega=0.0, L=None, R=None)
-
-#Ecrcc23, delta23 = calc_crcc23(T, L, Hbar, H, system, use_RHF=False)
+# )
+#
+# Hbar = build_hbar_ccsd(T, H)
+#
+# L, total_energy, is_converged = lcc_driver(calculation, system, T, Hbar, omega=0.0, L=None, R=None)
+#
+# Ecrcc23, delta23 = calc_crcc23(T, L, Hbar, H, system, use_RHF=False)
 
 
 
