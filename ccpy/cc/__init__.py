ca746e3867e22aaf7195ad4ebc6b364caf5b1258
Karthik Gururangan <gururang@msu.edu>
2022-03-17 14:29:11 -0400

    Added a prototype of Stephen's experimental iterative CR-CC(2,3) method.

--- ccpy/cc/__init__.py
+++ ccpy/cc/__init__.py
@@ -1,4 +1,4 @@
 
 
-__all__ = ["ccd", "ccsd", "ccsdt", "ccsdt_low_memory", "ccsdt1", "ccsdt2", "ccsdt3", "ccp", "accd"]
+__all__ = ["ccd", "ccsd", "ccsdt", "ccsdt_low_memory", "ccsdt1", "ccsdt2", "ccsdt3", "ccp", "accd", "icrcc23"]
 MODULES = [module for module in __all__]
