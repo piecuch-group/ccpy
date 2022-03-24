94c120f7addf93774eb6968a700e9efc9b318503
Karthik Gururangan <gururang@msu.edu>
2022-03-23 20:16:13 -0400

    Working EOMCCSDT. Had to revive the CCSDT HBar module.

--- examples/chplus_olsen/chplus_Re.py
+++ examples/chplus_olsen/chplus_Re.py
@@ -3,7 +3,7 @@ from ccpy.models.calculation import Calculation
 from ccpy.interfaces.gamess_tools import load_from_gamess
 from ccpy.drivers.driver import cc_driver, lcc_driver, eomcc_driver
 
-from ccpy.hbar.hbar_ccsd import build_hbar_ccsd
+from ccpy.hbar.hbar_ccsdt import build_hbar_ccsdt
 
 from ccpy.eomcc.initial_guess import get_initial_guess
 
@@ -23,7 +23,7 @@ if __name__ == "__main__":
 
     T, total_energy, is_converged = cc_driver(calculation, system, H)
 
-    Hbar = build_hbar_ccsd(T, H)
+    Hbar = build_hbar_ccsdt(T, H)
 
     calculation = Calculation(
         order=3,
