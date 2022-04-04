9fec5f40c35a1b602e17479369984318b83f901b
Karthik Gururangan <gururang@msu.edu>
2022-04-04 09:35:45 -0400

    Had to undo some changes in plugging in optimized CCSD routine in CCSDT. Need to be careful about vovv and ooov terms.
    Former-commit-id: 968fafcea763f248526116098a1eb75113082a38

--- ccpy/hbar/hbar_ccsd.py
+++ ccpy/hbar/hbar_ccsd.py
@@ -516,37 +516,3 @@ def get_ccsd_intermediates(T, H0):
     )
 
     return H
-
-if __name__ == "__main__":
-    from pyscf import gto, scf
-
-    from ccpy.models.calculation import Calculation
-    from ccpy.interfaces.pyscf_tools import load_pyscf_integrals
-    from ccpy.drivers.cc import driver
-
-    mol = gto.Mole()
-    mol.build(
-        atom="""F 0.0 0.0 -2.66816
-                F 0.0 0.0  2.66816""",
-        basis="ccpvdz",
-        charge=0,
-        spin=0,
-        symmetry="D2H",
-        cart=True,
-        unit='Bohr',
-    )
-    mf = scf.ROHF(mol)
-    mf.kernel()
-
-    system, H0 = load_pyscf_integrals(mf, nfrozen=2)
-    system.print_info()
-
-    calculation = Calculation(
-        order=2,
-        calculation_type="ccsd",
-        convergence_tolerance=1.0e-08,
-    )
-
-    T, total_energy, is_converged = driver(calculation, system, H0)
-
-    H = build_hbar_ccsd(T, H0)
