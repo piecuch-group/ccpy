ce93659e6f78f7425f2c77a0db2a508018cb4c0b
Karthik Gururangan <gururang@msu.edu>
2022-04-21 19:09:46 -0400

    Put in the "moving average" new DIIS scheme. Seemed to improve ground-state CC convergence. Not sure about left-CC.

--- examples/f2_vdz_ccsd.py
+++ examples/f2_vdz_ccsd.py
@@ -60,21 +60,22 @@ def main(args):
         num_active=num_active,
         convergence_tolerance=1.0e-08,
         diis_size=6,
+        maximum_iterations=80
     )
 
     T, total_energy, is_converged = cc_driver(calculation, system, H)
 
-    calculation = Calculation(
-        order=2,
-        calculation_type="left_ccsd",
-        convergence_tolerance=1.0e-08
-    )
-
-    Hbar = build_hbar_ccsd(T, H)
-
-    L, total_energy, is_converged = lcc_driver(calculation, system, T, Hbar, omega=0.0, L=None, R=None)
-
-    Ecrcc23, delta23 = calc_crcc23(T, L, Hbar, H, system, use_RHF=False)
+    # calculation = Calculation(
+    #     order=2,
+    #     calculation_type="left_ccsd",
+    #     convergence_tolerance=1.0e-08
+    # )
+    #
+    # Hbar = build_hbar_ccsd(T, H)
+    #
+    # L, total_energy, is_converged = lcc_driver(calculation, system, T, Hbar, omega=0.0, L=None, R=None)
+    #
+    # Ecrcc23, delta23 = calc_crcc23(T, L, Hbar, H, system, use_RHF=False)
 
 
 
