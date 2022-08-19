6fa8c08413f4176a6e40602c42d94458c7c70bbe
Karthik Gururangan <gururang@msu.edu>
2022-08-19 10:29:04 -0400

    Added CCSDT parts to 1RDM code.

--- ccpy/density/rdm1.py
+++ ccpy/density/rdm1.py
@@ -62,4 +62,64 @@ def calc_rdm1(T, L, system):
     rdm1.a.vo += T.a
     rdm1.b.vo += T.b
 
+    # CCSDT parts
+    if hasattr(T, 'aaa') and hasattr(L, 'aaa'):
+        
+        # oo block: <j|gamma|i> => i->-.->-j
+        rdm1.a.oo += (
+                    - (1.0 / 12.0) * np.einsum('efgino,efgjno->ij', L.aaa, T.aaa)
+                    - 0.5 * np.einsum('efgino,efgjno->ij', L.aab, T.aab)
+                    - 0.25 * np.einsum('efgino,efgjno->ij', L.abb, T.abb)
+        )
+        rdm1.b.oo += (
+                    - (1.0 / 12.0) * np.einsum('efgoni,efgonj->ij', L.bbb, T.bbb)
+                    - 0.5 * np.einsum('efgoni,efgonj->ij', L.abb, T.abb)
+                    - 0.25 * np.einsum('efgoni,efgonj->ij', L.aab, T.aab)
+        )
+
+        # vv block: <a|gamma|b> => a-<-.-<-b
+        rdm1.a.vv += (
+                    + (1.0 / 12.0) * np.einsum('bfgmno,afgmno->ab', L.aaa, T.aaa)
+                    + 0.5 * np.einsum('bfgmno,afgmno->ab', L.aab, T.aab)
+                    + 0.25 * np.einsum('bfgmno,afgmno->ab', L.abb, T.abb)
+        )
+        rdm1.b.vv += (
+                    + (1.0 / 12.0) * np.einsum('gfbmno,gfamno->ab', L.bbb, T.bbb)
+                    + 0.5 * np.einsum('gfbmno,gfamno->ab', L.abb, T.abb)
+                    + 0.25 * np.einsum('gfbmno,gfamno->ab', L.aab, T.aab)
+        )
+
+        # ov block: <a|gamma|i> .<ia
+        rdm1.a.vo += (
+                    + 0.25 * np.einsum('efmn,aefimn->ai', L.aa, T.aaa)
+                    + np.einsum('efmn,aefimn->ai', L.ab, T.aab)
+                    + 0.25 * np.einsum('efmn,aefimn->ai', L.bb, T.abb)
+                    - 0.25 * np.einsum('efgmno,agmo,efin->ai', L.aaa, T.aa, T.aa)
+                    - 0.5 * np.einsum('efgmno,agmo,efin->ai', L.aab, T.ab, T.aa)
+                    - 0.5 * np.einsum('egfmon,agmo,efin->ai', L.aab, T.aa, T.ab)
+                    - np.einsum('efgmno,agmo,efin->ai', L.abb, T.ab, T.ab)
+                    - (1.0 / 12.0) * np.einsum('efgmno,afgmno,ei->ai', L.aaa, T.aaa, T.a)
+                    - 0.5 * np.einsum('efgmno,afgmno,ei->ai', L.aab, T.aab, T.a)
+                    - 0.25 * np.einsum('efgmno,afgmno,ei->ai', L.abb, T.abb, T.a)
+                    - (1.0 / 12.0) * np.einsum('efgmno,efgino,am->ai', L.aaa, T.aaa, T.a)
+                    - 0.5 * np.einsum('efgmno,efgino,am->ai', L.aab, T.aab, T.a)
+                    - 0.25 * np.einsum('efgmno,efgino,am->ai', L.abb, T.abb, T.a)
+        )
+        rdm1.b.vo += (
+                    + 0.25 * np.einsum('efmn,aefimn->ai', L.bb, T.bbb)
+                    + np.einsum('efmn,efamni->ai', L.ab, T.abb)
+                    + 0.25 * np.einsum('efmn,efamni->ai', L.aa, T.aab)
+                    - 0.25 * np.einsum('efgmno,agmo,efin->ai', L.bbb, T.bb, T.bb)
+                    - 0.5 * np.einsum('fegnmo,agmo,feni->ai', L.abb, T.bb, T.ab)
+                    - 0.5 * np.einsum('gefomn,gaom,efin->ai', L.abb, T.ab, T.bb)
+                    - np.einsum('fgenom,gaom,feni->ai', L.aab, T.ab, T.ab)
+                    - (1.0 / 12.0) * np.einsum('efgmno,afgmno,ei->ai', L.bbb, T.bbb, T.b)
+                    - 0.5 * np.einsum('fgenom,fganom,ei->ai', L.abb, T.abb, T.b)
+                    - 0.25 * np.einsum('fgenom,fganom,ei->ai', L.aab, T.aab, T.b)
+                    - (1.0 / 12.0) * np.einsum('efgmno,efgino,am->ai', L.bbb, T.bbb, T.b)
+                    - 0.5 * np.einsum('fgenom,fgenoi,am->ai', L.abb, T.abb, T.b)
+                    - 0.25 * np.einsum('fgenom,fgenoi,am->ai', L.aab, T.aab, T.b)
+        )
+
     return rdm1
+
