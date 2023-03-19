6577bb257c0694d467ab2a894a3e57547ff1903d
Karthik Gururangan <gururang@linked.cem.msu.edu>
2023-03-19 10:39:39 -0400

    Closed-shell optimized CC(P) is working and it's speed is even better than the previous CC(P). Tested on CBD-TS and results are correct.

--- examples/adaptive_example/f2_adapt_ccsdt_lin.py
+++ examples/adaptive_example/f2_adapt_ccsdt_lin.py
@@ -66,7 +66,7 @@ def test_pyscf(stretch, basis):
             maximum_iterations=500,
             RHF_symmetry=False,
             low_memory=False,
-            adaptive_percentages=[10.0]
+            adaptive_percentages=[90.0]
             #adaptive_percentages=[1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
             #adaptive_percentages=[1.0, 5.0]
     )
