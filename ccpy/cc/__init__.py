3420d8b2caea3181f3fffad1d7e079bbe76e1362
Karthik Gururangan <gururang@coupled.cem.msu.edu>
2023-04-06 01:46:42 -0400

    Cleaned up some file structure.

--- ccpy/cc/__init__.py
+++ ccpy/cc/__init__.py
@@ -1,6 +1,2 @@
-
-
-__all__ = ["ccd", "ccsd", "ccsdt", "ccsdtq", "ccsdt1", "ccsdt2", "ccsdt3", "ccp", "accd", "icrcc23", "ccsdt_p", "ccsdt_p_slow",
-           "eccc2", "ccsdt_p_linear", "ccsdt_p_linear_omp", "ccsdt_p_quadratic", "ccsdt_p_quadratic_omp", "ccsdt_p_quadratic_direct",
-           "ccsdt_p_quadratic_direct_opt"]
+__all__ = ["ccd", "ccsd", "accd", "ccsdt", "ccsdtq", "ccsdt1", "ccsdt_p", "eccc2"]
 MODULES = [module for module in __all__]
