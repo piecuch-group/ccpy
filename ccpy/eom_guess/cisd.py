eea42d0a24b4072a41de10d1f126990e1bd3585d
Karthik Gururangan <gururang@msu.edu>
2023-09-18 14:52:54 -0400

    Optimized CISD S2 matrix construction. Works for singlets and doublets, but not for higher spins. Fixing...

--- ccpy/eom_guess/cisd.py
+++ ccpy/eom_guess/cisd.py
@@ -23,15 +23,19 @@ def run_diagonalization(system, H, multiplicity, roots_per_irrep, nacto, nactu, 
     print("   Active unoccupied alpha = ", min(nactu, nua))
     print("   Active unoccupied beta = ", min(nactu + (system.multiplicity - 1), nub))
 
-    t1 = time.time()
-    S2mat = build_s2matrix_cisd(system, nacto, nactu)
-    print("   Time requried for S2 matrix =", time.time() - t1, "seconds")
+    #t1 = time.time()
+    #S2mat, _, _, _, _, _, _, _, _, _, _ = build_s2matrix_cisd(system, nacto, nactu)
+    #print("   Time requried for S2 matrix =", time.time() - t1, "seconds")
 
     for irrep, nroot in roots_per_irrep.items():
         if nroot == 0: continue
         if not use_symmetry: irrep = None
+
+        # Build the indexing arrays for the given irrep
+        idx_a, idx_b, idx_aa, idx_ab, idx_bb = get_index_arrays(nacto, nactu, system, irrep)
         t1 = time.time()
-        Hmat = build_cisd_hamiltonian(H, system, nacto, nactu, irrep)
+        Hmat = build_cisd_hamiltonian(H, nacto, nactu, idx_a, idx_b, idx_aa, idx_ab, idx_bb, system)
+        S2mat, _, _, _, _, _, _, _, _, _, _ = build_s2matrix(system, nacto, nactu, idx_a, idx_b, idx_aa, idx_ab, idx_bb)
         t2 = time.time()
         omega, V_act = spin_adapt_guess(S2mat, Hmat, multiplicity, debug=debug)
 
@@ -46,6 +50,22 @@ def run_diagonalization(system, H, multiplicity, roots_per_irrep, nacto, nactu, 
             if kout == nroot:
                 break
 
+        # t1 = time.time()
+        # Hmat = build_cisd_hamiltonian(H, system, nacto, nactu, irrep)
+        # t2 = time.time()
+        # omega, V_act = spin_adapt_guess(S2mat, Hmat, multiplicity, debug=debug)
+        #
+        # nroot = min(nroot, V_act.shape[1])
+        # kout = 0
+        # for i in range(len(omega)):
+        #     if omega[i] == 0.0: continue
+        #     V[:, n_found] = scatter(V_act[:, i], nacto, nactu, system)
+        #     omega_guess[n_found] = omega[i]
+        #     n_found += 1
+        #     kout += 1
+        #     if kout == nroot:
+        #         break
+
         print("   -----------------------------------")
         print("   Target symmetry irrep = ", irrep, f"({system.point_group})")
         print("   Dimension of eigenvalue problem = ", V_act.shape[0])
@@ -56,6 +76,7 @@ def run_diagonalization(system, H, multiplicity, roots_per_irrep, nacto, nactu, 
     return omega_guess, V
 
 def scatter(V_in, nacto, nactu, system):
+#def scatter(V_act[:, i], nacto, nactu, idx_a, idx_b, idx_aa, idx_ab, idx_bb, system):
     # orbital dimensions
     noa = system.noccupied_alpha
     nob = system.noccupied_beta
@@ -113,7 +134,7 @@ def scatter(V_in, nacto, nactu, system):
                         offset += 1
     return np.hstack((V_a_out.flatten(), V_b_out.flatten(), V_aa_out.flatten(), V_ab_out.flatten(), V_bb_out.flatten()))
 
-def build_cisd_hamiltonian(H, system, nacto, nactu, target_irrep):
+def build_cisd_hamiltonian(H, nacto, nactu, idx_a, idx_b, idx_aa, idx_ab, idx_bb, system):
 
     noa, nob, nua, nub = H.ab.oovv.shape
 
@@ -132,7 +153,7 @@ def build_cisd_hamiltonian(H, system, nacto, nactu, target_irrep):
     # total dimension
     ndim = n1a + n1b + n2a + n2b + n2c
 
-    idx_a, idx_b, idx_aa, idx_ab, idx_bb = get_index_arrays(nacto, nactu, system, target_irrep)
+    #idx_a, idx_b, idx_aa, idx_ab, idx_bb = get_index_arrays(nacto, nactu, system, target_irrep)
 
     ####################################################################################
     # ALPHA SINGLES
@@ -705,6 +726,209 @@ def build_cisd_hamiltonian(H, system, nacto, nactu, target_irrep):
          np.concatenate((a_H_bb.T, bb_H_b, aa_H_bb.T, bb_H_ab, bb_H_bb), axis=1)
          ), axis=0
     )
+
+def get_sz2(system, Ms):
+    Ns = float((system.noccupied_alpha + Ms) - (system.noccupied_beta - Ms))
+    sz = Ns / 2.0
+    sz2 = (sz + 1.0) * sz
+    return sz2
+
+def build_s2matrix(system, nacto, nactu, idx_a, idx_b, idx_aa, idx_ab, idx_bb):
+
+    # orbital dimensions
+    noa = system.noccupied_alpha
+    nua = system.nunoccupied_alpha
+    nob = system.noccupied_beta
+    nub = system.nunoccupied_beta
+
+    # set active space parameters
+    nacto_a = min(nacto + (system.multiplicity - 1), system.noccupied_alpha)
+    nacto_b = min(nacto, system.noccupied_beta)
+    nactu_a = min(nactu, system.nunoccupied_alpha)
+    nactu_b = min(nactu + (system.multiplicity - 1), system.nunoccupied_beta)
+
+    def chi_beta(p):
+        if p >= 0 and p < system.noccupied_beta:
+            return 1.0
+        else:
+            return 0.0
+
+    def pi_alpha(p):
+        if p >= system.noccupied_alpha and p < system.nunoccupied_alpha + system.noccupied_alpha:
+            return 1.0
+        else:
+            return 0.0
+
+    n1a = system.nunoccupied_alpha * system.noccupied_alpha
+    n1b = system.nunoccupied_beta * system.noccupied_beta
+    n2a = int(nactu_a * (nactu_a - 1) / 2 * nacto_a * (nacto_a - 1) / 2)
+    n2b = nacto_a * nacto_b * nactu_a * nactu_b
+    n2c = int(nactu_b * (nactu_b - 1) / 2 * nacto_b * (nacto_b - 1) / 2)
+
+    #
+    ndocc = nob
+    docc = range(nob)
+    nsocc = noa - nob
+    socc = range(nob, noa)
+    sz2 = get_sz2(system, Ms=0)
+    #
+    a_S_a = np.zeros((n1a, n1a))
+    a_S_b = np.zeros((n1a, n1b))
+    a_S_ab = np.zeros((n1a, n2b))
+    for a in range(nua):
+        for i in range(noa):
+            idet = idx_a[a, i]
+            if idet == 0: continue
+            I = abs(idet) - 1
+            # < ia | S2 | ia >
+            a_S_a[I, I] += (sz2 + 1.0 * chi_beta(i))
+            # < ia | S2 | j~b~ > = -delta(i,j~) * delta(a,b~)
+            if chi_beta(i):
+                jdet = idx_b[a + nsocc, i]
+                if jdet != 0:
+                    J = abs(jdet) - 1
+                    a_S_b[I, J] -= 1.0
+            # < ia | S2 | BC~JK~ > = delta(a,B)*delta(i,K~)*delta(J,C~)
+            if i < nob and i >= nob - nacto_b and a < nactu_a:
+                for s in socc:
+                    jdet = idx_ab[a, s - nob, s, i]
+                    if jdet != 0:
+                        J = abs(jdet) - 1
+                        a_S_ab[I, J] += 1.0
+    #
+    b_S_b = np.zeros((n1b, n1b))
+    b_S_ab = np.zeros((n1b, n2b))
+    for a in range(nub):
+        for i in range(nob):
+            idet = idx_b[a, i]
+            if idet == 0: continue
+            I = abs(idet) - 1
+            # < i~a~ | S2 | i~a~ >
+            b_S_b[I, I] += (sz2 + 1.0 * pi_alpha(a + nob))
+            # < i~a~ | S2 | BC~JK~ > = -delta(i~,K)*delta(J,C~)*delta(a~,B)
+            if a >= nsocc:
+                for s in socc:
+                    jdet = idx_ab[a - nsocc, s - nob, s, i]
+                    if jdet != 0:
+                        J = abs(jdet) - 1
+                        b_S_ab[I, J] -= 1.0
+    #
+    aa_S_aa = np.zeros((n2a, n2a))
+    aa_S_ab = np.zeros((n2a, n2b))
+    for A in range(nactu_a):
+        for B in range(A + 1, nactu_a):
+            for I in range(noa - nacto_a, noa):
+                for J in range(I + 1, noa):
+                    idet = idx_aa[A, B, I, J]
+                    if idet == 0: continue
+                    ind1 = abs(idet) - 1
+                    # < IJAB | S2 | IJAB > = +A(IJ)A(KL) d(A,C) d(J,L) d(B,D) d(I,K) chi_beta(I)
+                    aa_S_aa[ind1, ind1] += (sz2 + chi_beta(J) + chi_beta(I))
+                    # < IJAB | S2 | KL~CD~ > = -A(IJ)A(AB) d(I,K) d(A,C) d(L~,J) d(B,D~)
+                    if J < nob and J >= nob - nacto_b: # (1)
+                        jdet = idx_ab[A, B + nsocc, I, J]
+                        if jdet != 0:
+                            ind2 = abs(jdet) - 1
+                            aa_S_ab[ind1, ind2] -= 1.0
+
+                        jdet = idx_ab[B, A + nsocc, I, J]
+                        if jdet != 0:
+                            ind2 = abs(jdet) - 1
+                            aa_S_ab[ind1, ind2] += 1.0
+
+                    if I < nob and I >= nob - nacto_b: # (IJ)
+                        jdet = idx_ab[A, B + nsocc, J, I]
+                        if jdet != 0:
+                            ind2 = abs(jdet) - 1
+                            aa_S_ab[ind1, ind2] += 1.0
+
+                        jdet = idx_ab[B, A + nsocc, J, I]
+                        if jdet != 0:
+                            ind2 = abs(jdet) - 1
+                            aa_S_ab[ind1, ind2] -= 1.0
+    #
+    ab_S_ab = np.zeros((n2b, n2b))
+    ab_S_bb = np.zeros((n2b, n2c))
+    for A in range(nactu_a):
+        for B in range(nactu_b):
+            for I in range(noa - nacto_a, noa):
+                for J in range(nob - nacto_b, nob):
+                    idet = idx_ab[A, B, I, J]
+                    if idet == 0: continue
+                    ind1 = abs(idet) - 1
+                    # < IJ~AB~ | S2 | KL~CD~ >
+                    # sz2 * (I == K) * (J == L) * (A == C) * (B == D)
+                    # +d(J,L) d(A,C) d(I,K) d(B,D) pi_alpha(B)
+                    # +d(I,K) d(J,L) d(B,D) d(A,C) chi_beta(I)
+                    ab_S_ab[ind1, ind1] += (sz2 + pi_alpha(B + nob) + chi_beta(I))
+                    # -d(A,C) d(B~,D~) <L~|I> <K|J~>
+                    if chi_beta(I):
+                        jdet = idx_ab[A, B, J, I]
+                        if jdet != 0:
+                            ind2 = abs(jdet) - 1
+                            ab_S_ab[ind1, ind2] -= 1.0
+                    # -d(I,K) d(J~,L~) <A|D~> <B~|C>
+                    if pi_alpha(B + nob):
+                        jdet = idx_ab[B - nsocc, A + nsocc, I, J]
+                        if jdet != 0:
+                            ind2 = abs(jdet) - 1
+                            ab_S_ab[ind1, ind2] -= 1.0
+                    # +d(J~,L~) d(A,C) <K|D~> <B~|I>
+                    ### THIS WORKS FOR DOUBLETS (nsocc=1) but not for nsocc>1
+                    # if I in socc and B + nob in socc:
+                    #    for s in socc:
+                    #        jdet = idx_ab[A, I - nob, s, J]
+                    #        if jdet != 0:
+                    #           ind2 = abs(jdet) - 1
+                    #           ab_S_ab[ind1, ind2] += 1.0
+                    ###
+                    if I in socc and B + nob in socc:
+                        for s in socc:
+                            jdet = idx_ab[A, s - nob, s, J]
+                            if jdet != 0:
+                                ind2 = abs(jdet) - 1
+                                ab_S_ab[ind2, ind2] += 1.0
+
+                    # < IJ~AB~ | S2 | K~L~C~D~ >
+                    # -A(K~L~)A(C~D~) d(J~,L~) d(B~,D~) <K~|I> <A|C~>
+                    if chi_beta(I) == 1.0:
+                        # -d(J~,L~) d(B~,D~) <K~|I> <A|C~> (1)
+                        jdet = idx_bb[A + nsocc, B, I, J]
+                        if jdet != 0:
+                            ind2 = abs(jdet) - 1
+                            phase = np.sign(jdet)
+                            ab_S_bb[ind1, ind2] -= 1.0 * phase
+    #
+    bb_S_bb = np.zeros((n2c, n2c))
+    for A in range(nactu_b):
+        for B in range(A + 1, nactu_b):
+            for I in range(nob - nacto_b, nob):
+                for J in range(I + 1, nob):
+                    idet = idx_bb[A, B, I, J]
+                    if idet == 0: continue
+                    ind1 = abs(idet) - 1
+                    # < I~J~A~B~ | S2 | I~J~A~B~ > = +A(IJ)A(KL) d(A,C) d(J,L) d(B,D) d(I,K) chi_beta(I)
+                    bb_S_bb[ind1, ind1] += (sz2 + pi_alpha(A + nob) + pi_alpha(B + nob))
+    # Lot of zero blocks
+    a_S_aa = np.zeros((n1a, n2a))
+    aa_S_a = a_S_aa.T
+    b_S_aa = np.zeros((n1b, n2a))
+    aa_S_b = b_S_aa.T
+    b_S_bb = np.zeros((n1b, n2c))
+    bb_S_b = b_S_bb.T
+    a_S_bb = np.zeros((n1a, n2c))
+    bb_S_a = a_S_bb.T
+    aa_S_bb = np.zeros((n2a, n2c))
+    bb_S_aa = aa_S_bb.T
+
+    S2mat = np.concatenate(
+        (np.concatenate((a_S_a, a_S_b, a_S_aa, a_S_ab, a_S_bb), axis=1),
+         np.concatenate((a_S_b.T, b_S_b, b_S_aa, b_S_ab, b_S_bb), axis=1),
+         np.concatenate((aa_S_a, aa_S_b, aa_S_aa, aa_S_ab, aa_S_bb), axis=1),
+         np.concatenate((a_S_ab.T, b_S_ab.T, aa_S_ab.T, ab_S_ab, ab_S_bb), axis=1),
+         np.concatenate((bb_S_a, bb_S_b, bb_S_aa, ab_S_bb.T, bb_S_bb), axis=1)
+         ), axis=0)
+    return S2mat, a_S_a, a_S_b, a_S_ab, b_S_b, b_S_ab, aa_S_aa, aa_S_ab, ab_S_ab, ab_S_bb, bb_S_bb
 
 def get_index_arrays(nacto, nactu, system, target_irrep):
 
