c6e2fa3a20788847099f0c8abc99946582bcbf90
Karthik Gururangan <gururang@msu.edu>
2023-11-27 21:27:03 -0500

    Removed reorder routine to align L3 with T3/R3 at the end of left-CC(P)/left-EOMCC(P) routines. It so happens that L3 is already aligned to these vectors, and this alignment was being performed in an extremely inefficient Ndet^2 step that took up too much time.

--- ccpy/energy/cc_energy.py
+++ ccpy/energy/cc_energy.py
@@ -132,15 +132,21 @@ def get_LR(R, L, l3_excitations=None, r3_excitations=None):
                 LR += (1.0 / 4.0) * np.einsum("efgmno,efgmno->", R.abb, L.abb, optimize=True)
                 LR += (1.0 / 36.0) * np.einsum("efgmno,efgmno->", R.bbb, L.bbb, optimize=True)
             else:
-                LR_P = leftccsdt_p_loops.leftccsdt_p_loops.lr(L.aaa, l3_excitations["aaa"],
-                                                              L.aab, l3_excitations["aab"],
-                                                              L.abb, l3_excitations["abb"],
-                                                              L.bbb, l3_excitations["bbb"],
-                                                              R.aaa, r3_excitations["aaa"],
-                                                              R.aab, r3_excitations["aab"],
-                                                              R.abb, r3_excitations["abb"],
-                                                              R.bbb, r3_excitations["bbb"],
-                                                              noa, nua, nob, nub)
+                # LR_P = leftccsdt_p_loops.leftccsdt_p_loops.lr(L.aaa, l3_excitations["aaa"],
+                #                                               L.aab, l3_excitations["aab"],
+                #                                               L.abb, l3_excitations["abb"],
+                #                                               L.bbb, l3_excitations["bbb"],
+                #                                               R.aaa, r3_excitations["aaa"],
+                #                                               R.aab, r3_excitations["aab"],
+                #                                               R.abb, r3_excitations["abb"],
+                #                                               R.bbb, r3_excitations["bbb"],
+                #                                               noa, nua, nob, nub)
+                # This is allowed because L and R are always aligned through the left-EOMCC(P) iterations
+                LR_aaa = np.dot(L.aaa.T, R.aaa)
+                LR_aab = np.dot(L.aab.T, R.aab)
+                LR_abb = np.dot(L.abb.T, R.abb)
+                LR_bbb = np.dot(L.bbb.T, R.bbb)
+                LR_P = LR_aaa + LR_aab + LR_abb + LR_bbb
                 LR += LR_P
     elif isinstance(L, FockOperator):
         LR = -np.einsum("m,m->", R.a, L.a, optimize=True)
