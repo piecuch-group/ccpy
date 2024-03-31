a10873eefc2881458c40f09cede49af48602747c
Karthik Gururangan <gururang@msu.edu>
2024-03-30 21:39:07 -0400

    Updated OH radical IP-EOMCCSD(3h-2p) test to reflect the example used in JCP 123 (2005). I'm able to match the results for the 1p roots that I can pick up.

--- tests/ohminus/test_ipeom3_ohminus.py
+++ tests/ohminus/test_ipeom3_ohminus.py
@@ -1,32 +1,36 @@
-"""IP-EOMCCSD(3h-2p) computation to describe the spectrum of the open-shell
-OH molecule described by ionizing an electron from the closed-shell (OH)- ion."""
+"""
+IP-EOMCCSD(3h-2p) calculation to obtain the vertical excitation spectrum
+of the open-shell OH radical, as by described by the 6-31G** basis set, by
+removing one electron from the (OH)- closed shell.
+Reference: J. Chem. Phys. 123, 134113 (2005)
+"""
 
 import numpy as np
 from pyscf import scf, gto
 from ccpy.drivers.driver import Driver
 
 def test_ipeom3_ohminus():
-    mol = gto.M(atom='''O  0.0  0.0  -0.8
-                        H  0.0  0.0   0.8''',
-                basis="cc-pvdz",
+    mol = gto.M(atom='''O  0.0  0.0  -0.96966/2
+                        H  0.0  0.0   0.96966/2''',
+                basis="6-31g**",
                 charge=-1,
                 spin=0,
                 cart=False,
                 symmetry="C2V")
     mf = scf.RHF(mol)
     mf.kernel()
-    driver = Driver.from_pyscf(mf, nfrozen=0)
+    driver = Driver.from_pyscf(mf, nfrozen=1)
     driver.system.print_info()
 
     driver.run_cc(method="ccsd")
     driver.run_hbar(method="ccsd")
-    driver.run_guess(method="ipcis", multiplicity=2, roots_per_irrep={"A1": 5, "B1": 0, "B2": 0, "A2": 0}, debug=False, use_symmetry=False)
-    driver.run_ipeomcc(method="ipeom3", state_index=[0, 1, 2, 3, 4])
+    driver.run_guess(method="ipcis", multiplicity=2, roots_per_irrep={"A1": 4, "B1": 0, "B2": 0, "A2": 0}, use_symmetry=False)
+    driver.run_ipeomcc(method="ipeom3", state_index=[0, 1, 2, 3])
 
     #
     # Check the results
     #
-    expected_vee = [-0.00814477, -0.00814477, 0.11305111, 0.62233209, 19.46600051]
+    expected_vee = [-0.01598430, -0.01598430, 0.14381607, 0.66870089]
     for i, vee in enumerate(expected_vee):
         assert np.allclose(driver.vertical_excitation_energy[i], vee)
 
--- tests/ohminus/test_ipeom3a_ohminus.py
+++ tests/ohminus/test_ipeom3a_ohminus.py
@@ -1,23 +1,28 @@
-"""Active-space IP-EOMCCSD(3h-2p){No} (also known as IP-EOMCCSDt) calculation to
-describe the vertical excitation spectrum of the open-shell OH radical by removing
-one electron from the (OH)- closed shell."""
+"""
+Active-space IP-EOMCCSD(3h-2p){No} (also known as IP-EOMCCSDt) calculation to
+obtain the vertical excitation spectrum of the open-shell OH radical, as
+by described by the 6-31G** basis set, by removing one electron from the
+(OH)- closed shell.
+Reference: J. Chem. Phys. 123, 134113 (2005)
+"""
 
 import numpy as np
 from pyscf import scf, gto
 from ccpy.drivers.driver import Driver
 from ccpy.utilities.pspace import get_active_3h2p_pspace
+from ccpy.constants.constants import hartreetoeV
 
 def test_ipeom3a_ohminus():
-    mol = gto.M(atom='''O  0.0  0.0  -0.8
-                        H  0.0  0.0   0.8''',
-                basis="cc-pvdz",
+    mol = gto.M(atom='''O  0.0  0.0  -0.96966/2
+                        H  0.0  0.0   0.96966/2''',
+                basis="6-31g**",
                 charge=-1,
                 spin=0,
                 cart=False,
                 symmetry="C2V")
     mf = scf.RHF(mol)
     mf.kernel()
-    driver = Driver.from_pyscf(mf, nfrozen=0)
+    driver = Driver.from_pyscf(mf, nfrozen=1)
     driver.system.print_info()
 
     # Perform CCSD for the closed-shell core
@@ -26,21 +31,22 @@ def test_ipeom3a_ohminus():
     driver.run_hbar(method="ccsd")
 
     # Set the number of active occupied orbitals used to define the r_{bc}^{ijK} operator
-    driver.system.set_active_space(nact_unoccupied=0, nact_occupied=3)
+    # No = 2 includes the 1π_x and 1π_y orbitals in the active space
+    driver.system.set_active_space(nact_unoccupied=0, nact_occupied=2)
     # Obtain the active-space 3h2p list
     r3_excitations = get_active_3h2p_pspace(driver.system, num_active=1)
 
     # Perform guess vectors by diagonalizaing within the 1h space (no restriction on spatial symmetry is used)
-    driver.run_guess(method="ipcis", multiplicity=2,
-                     roots_per_irrep={"A1": 5, "B1": 0, "B2": 0, "A2": 0}, use_symmetry=False)
+    driver.run_guess(method="ipcis", multiplicity=-1,
+                     roots_per_irrep={"A1": 4, "B1": 0, "B2": 0, "A2": 0}, use_symmetry=False)
     # Loop over all guess vectors and perform the IP-EOMCSDt calculation
-    for istate in [0, 1, 2, 3, 4]:
+    for istate in [0, 1, 2, 3]:
         driver.run_ipeomccp(method="ipeom3_p", state_index=istate, r3_excitations=r3_excitations)
 
     #
     # Check the results
     #
-    expected_vee = [-0.00814417, -0.00814417, 0.11305131, 0.62231423, 19.46549731]
+    expected_vee = [-0.01583013, -0.01583013, 0.14362613, 0.67650622]
     for i, vee in enumerate(expected_vee):
         assert np.allclose(driver.vertical_excitation_energy[i], vee)
 
