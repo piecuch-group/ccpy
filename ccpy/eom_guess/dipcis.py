Added CCSD(T)(a) method and associated HBar builder. Checked that these match with the code in Miniccpy (after making a bug fix that reorders the computation of h(vvov)/h(vooo) after updating T1/T2). Still not finding agreement in the DIP-EOMCC(4h-2p)T(a)* routine.

ccpy/eom_guess/dipcis.py
@@ -22,8 +22,8 @@ def run_diagonalization(system, H, multiplicity, roots_per_irrep, nacto, nactu, 
     #omega, V_act = spin_adapt_guess(S2mat, Hmat, multiplicity, debug=debug)
     omega, V_act = np.linalg.eig(Hmat)
     idx = np.argsort(omega)
-    omega = omega[idx]
-    V_act = V_act[:, idx]
+    omega = np.real(omega[idx])
+    V_act = np.real(V_act[:, idx])
 
     nroot = min(nroot, V_act.shape[1])
 