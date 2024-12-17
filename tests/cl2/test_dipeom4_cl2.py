cc7f2b56278bf3c67f1fc2736cafeaf2f70c4789
Karthik Gururangan <gururang@msu.edu>
2024-12-17 13:10:06 -0500

    Pushed tests for DIP-EOMCCSDT(4h-2p) and DIP-EOMCCSD(T)(a)(4h-2p).

--- tests/cl2/test_dipeom4_cl2.py
+++ tests/cl2/test_dipeom4_cl2.py
@@ -11,10 +11,8 @@ def test_dipeom4_cl2():
 
     mol = gto.M(atom=geom, basis="cc-pvdz", symmetry="D2H", unit="Angstrom", cart=False, charge=0)
     mf = scf.RHF(mol)
-    mf = mf.x2c()
+    mf = mf.x2c() # use SFX2C-1e scalar relativity
     mf.kernel()
-
-    print("   Using SFX2C-1e scalar relativity")
 
     driver = Driver.from_pyscf(mf, nfrozen=nfrozen)
     driver.system.print_info()
@@ -25,6 +23,13 @@ def test_dipeom4_cl2():
     driver.run_guess(method="dipcis", multiplicity=-1, nact_occupied=driver.system.noccupied_alpha, roots_per_irrep={"A1": 10}, use_symmetry=False) 
     driver.run_dipeomcc(method="dipeom4", state_index=[0, 1, 3, 4])
 
-if __name__ == "__main__":
+    #
+    # Check the results
+    #
+    assert np.allclose(driver.vertical_excitation_energy[0], 1.11368277, atol=1.0e-07, rtol=1.0e-07)  # X ^{3}Sigma_g-
+    assert np.allclose(driver.vertical_excitation_energy[1], 1.13436974, atol=1.0e-07, rtol=1.0e-07)  # a ^{1}Delta_g
+    assert np.allclose(driver.vertical_excitation_energy[3], 1.14727205, atol=1.0e-07, rtol=1.0e-07)  # b ^{1}Sigma_g+
+    assert np.allclose(driver.vertical_excitation_energy[4], 1.18607765, atol=1.0e-07, rtol=1.0e-07)  # c ^{1}Sigma_u-
 
+if __name__ == "__main__":
     test_dipeom4_cl2()
--- tests/cl2/test_dipeomccsdt_cl2.py
+++ tests/cl2/test_dipeomccsdt_cl2.py
@@ -1,4 +1,3 @@
-import argparse
 import numpy as np
 from pyscf import gto, scf
 from ccpy.drivers.driver import Driver
@@ -14,10 +13,8 @@ def test_dipeomccsdt_cl2():
 
     mol = gto.M(atom=geom, basis="cc-pvdz", symmetry="D2H", unit="Angstrom", cart=False, charge=0)
     mf = scf.RHF(mol)
-    mf = mf.x2c()
+    mf = mf.x2c() # Use SFX2C-1e treatment of scalar relativity
     mf.kernel()
-
-    print("   Using SFX2C-1e scalar relativity")
 
     driver = Driver.from_pyscf(mf, nfrozen=nfrozen)
     driver.system.print_info()
@@ -31,6 +28,13 @@ def test_dipeomccsdt_cl2():
     driver.run_guess(method="dipcis", multiplicity=-1, nact_occupied=driver.system.noccupied_alpha, roots_per_irrep={"A1": 10}, use_symmetry=False) 
     driver.run_dipeomcc(method="dipeomccsdt", state_index=[0, 1, 3, 4])
 
-if __name__ == "__main__":
+    #
+    # Check the results
+    #
+    assert np.allclose(driver.vertical_excitation_energy[0], 1.11738442, atol=1.0e-07, rtol=1.0e-07)  # X ^{3}Sigma_g-
+    assert np.allclose(driver.vertical_excitation_energy[1], 1.13790671, atol=1.0e-07, rtol=1.0e-07)  # a ^{1}Delta_g
+    assert np.allclose(driver.vertical_excitation_energy[3], 1.15068438, atol=1.0e-07, rtol=1.0e-07)  # b ^{1}Sigma_g+
+    assert np.allclose(driver.vertical_excitation_energy[4], 1.18912831, atol=1.0e-07, rtol=1.0e-07)  # c ^{1}Sigma_u-
 
+if __name__ == "__main__":
     test_dipeomccsdt_cl2()
