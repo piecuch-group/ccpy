9258ebb4002e664a0a2983c11224cf90785c424f
Karthik Gururangan <gururang@msu.edu>
2025-01-14 10:55:50 -0500

    Spatial symmetry adaptation to DIPCIS/DEACIS guess.

--- ccpy/eom_guess/deacis.py
+++ ccpy/eom_guess/deacis.py
@@ -2,10 +2,21 @@
 DEA-EOMCCSD(2p) Guess Routine for DEA-EOMCC
 '''
 
+import time
 import numpy as np
 from ccpy.eom_guess.s2matrix import spin_adapt_guess
 
 def run_diagonalization(system, H, multiplicity, roots_per_irrep, nacto, nactu, debug=False, use_symmetry=False):
+
+    nroots_total = 0
+    for key, value in roots_per_irrep.items():
+        nroots_total += value
+
+    noa, nob, nua, nub = H.ab.oovv.shape
+    ndim = nua*nub
+    V = np.zeros((ndim, nroots_total))
+    omega_guess = np.zeros(nroots_total)
+    n_found = 0
 
     # print results of initial guess procedure
     print("   DEA-CIS initial guess routine")
@@ -16,23 +27,39 @@ def run_diagonalization(system, H, multiplicity, roots_per_irrep, nacto, nactu, 
     print("   Active unoccupied alpha = ", min(nactu, system.nunoccupied_alpha))
     print("   Active unoccupied beta = ", min(nactu + (system.multiplicity - 1), system.nunoccupied_beta))
 
-    nroot = 0
-    for irrep, nroot_irrep in roots_per_irrep.items():
-        nroot += nroot_irrep
+    for irrep, nroot in roots_per_irrep.items():
+        if nroot == 0: continue
+        if not use_symmetry: irrep = None
 
-    Hmat = build_2p_hamiltonian(H, system, nactu)
-    S2mat = build_s2matrix_2p(system, nactu)
-    omega, V_act = spin_adapt_guess(S2mat, Hmat, multiplicity, debug=debug)
+        # Build the indexing arrays for the given irrep
+        idx_ab, ndim_irrep = get_index_arrays(nacto, nactu, system, irrep)
+        t1 = time.time()
+        # Compute the active-space 2p Hamiltonian
+        Hmat = build_2p_hamiltonian(H, nactu, system, idx_ab)
+        # Compute the S2 matrix in the same projection subspace
+        S2mat = build_s2matrix_2p(system, nactu, idx_ab)
+        # Project H onto the spin subspace with the specified multiplicity
+        omega, V_act = spin_adapt_guess(S2mat, Hmat, multiplicity, debug=debug)
+        nroot = min(nroot, V_act.shape[1])
+        kout = 0
+        for i in range(len(omega)):
+            if omega[i] == 0.0: continue
+            V[:, n_found] = scatter(V_act[:, i], nacto, system)
+            omega_guess[n_found] = omega[i]
+            n_found += 1
+            kout += 1
+            if kout == nroot:
+                break
 
-    nroot = min(nroot, V_act.shape[1])
-
-    # scatter active-space guess into full space
-    V = np.zeros((system.nunoccupied_alpha*system.nunoccupied_beta, nroot))
-    for i in range(nroot):
-        V[:, i] = scatter(V_act[:, i], nactu, system)
-        print("   Eigenvalue of root", i + 1, " = ", np.round(omega[i], 8))
+        elapsed_time = time.time() - t1
+        print("   -----------------------------------")
+        print("   Target symmetry irrep = ", irrep, f"({system.point_group})")
+        print("   Dimension of eigenvalue problem = ", ndim_irrep)
+        print("   Elapsed time = ", np.round(elapsed_time, 2), "seconds")
+        for i in range(n_found - kout, n_found):
+            print("   Eigenvalue of root", i + 1, " = ", np.round(omega_guess[i], 8))
     print("")
-    return omega, V
+    return omega_guess, V
 
 def scatter(V_in, nactu, system):
 
@@ -48,7 +75,7 @@ def scatter(V_in, nactu, system):
             ct += 1
     return V_out
 
-def build_2p_hamiltonian(H, system, nactu):
+def build_2p_hamiltonian(H, nactu, system, idx_ab):
 
     noa = system.noccupied_alpha
     nob = system.noccupied_beta
@@ -63,20 +90,21 @@ def build_2p_hamiltonian(H, system, nactu):
     n2b = nactu_a * nactu_b
 
     Hab = np.zeros((n2b, n2b))
-    ct1 = 0
     for a in range(nactu_a):
         for b in range(nactu_b):
-            ct2 = 0
+            idet = idx_ab[a, b]
+            if idet == 0: continue
+            ind1 = abs(idet) - 1
             for c in range(nactu_a):
                 for d in range(nactu_b):
-                    Hab[ct1, ct2] = (
-                          H.b.vv[b, d] * (a == c)
-                        + H.a.vv[a, c] * (b == d)
-                        + H.ab.vvvv[a, b, c, d]
-                    )
-                    ct2 += 1
-            ct1 += 1
-
+                    jdet = idx_ab[c, d]
+                    if jdet != 0:
+                        ind2 = abs(jdet) - 1
+                        Hab[ind1, ind2] = (
+                              H.b.vv[b, d] * (a == c)
+                            + H.a.vv[a, c] * (b == d)
+                            + H.ab.vvvv[a, b, c, d]
+                        )
     return Hab
 
 def get_sz2(system, Ms):
@@ -85,7 +113,7 @@ def get_sz2(system, Ms):
     sz2 = (sz + 1.0) * sz
     return sz2
 
-def build_s2matrix_2p(system, nactu):
+def build_s2matrix_2p(system, nactu, idx_ab):
 
     def pi_alpha(p):
         if p >= system.noccupied_alpha and p < system.nunoccupied_alpha + system.noccupied_alpha:
@@ -106,14 +134,49 @@ def build_s2matrix_2p(system, nactu):
     n2b = nactu_a * nactu_b
     sz2 = get_sz2(system, Ms=0) # this needs to be modified potentially
     Sab = np.zeros((n2b, n2b))
-    ct1 = 0
     for a in range(system.noccupied_alpha, system.noccupied_alpha + nactu_a):
         for b in range(system.noccupied_beta, system.noccupied_beta + nactu_b):
-            ct2 = 0
+            idet = idx_ab[a, b]
+            if idet == 0: continue
+            ind1 = abs(idet) - 1
             for c in range(system.noccupied_alpha, system.noccupied_alpha + nactu_a):
                 for d in range(system.noccupied_beta, system.noccupied_beta + nactu_b):
-                    Sab[ct1, ct2] += (sz2 + 1.0 * pi_alpha(a)) * (a == c) * (b == d)
-                    Sab[ct1, ct2] -= (b == c) * (a == d) # why is this a minus sign, you ask... "ghost loop" rule
-                    ct2 += 1
-            ct1 += 1
+                    jdet = idx_ab[c, d]
+                    if jdet != 0:
+                        ind2 = abs(jdet) - 1
+                        Sab[ind1, ind2] += (sz2 + 1.0 * pi_alpha(a)) * (a == c) * (b == d)
+                        Sab[ind1, ind2] -= (b == c) * (a == d) # why is this a minus sign, you ask... "ghost loop" rule
     return Sab
+
+def get_index_arrays(nacto, nactu, system, target_irrep):
+
+    noa = system.noccupied_alpha
+    nua = system.nunoccupied_alpha
+    nob = system.noccupied_beta
+    nub = system.nunoccupied_beta
+
+    # set active space parameters
+    # nacto_a = min(nacto + (system.multiplicity - 1), noa)
+    # nacto_b = min(nacto, nob)
+    nactu_a = min(nactu, nua)
+    nactu_b = min(nactu + (system.multiplicity - 1), nub)
+
+    if target_irrep is None:
+        sym1 = lambda a, b: True
+    else:
+        sym = lambda orbital_number: system.point_group_irrep_to_number[system.orbital_symmetries[orbital_number]]
+        ref_sym = system.point_group_irrep_to_number[system.reference_symmetry]
+        target_sym = system.point_group_irrep_to_number[target_irrep]
+
+        sym1 = lambda a, b: sym(a + noa) ^ sym(b + nob) ^ ref_sym == target_sym
+
+    ndim = 0
+    idx_ab = np.zeros((nua, nub), dtype=np.int32)
+    ct = 1
+    for a in range(nactu_a):
+        for b in range(nactu_b):
+            if sym1(a, b):
+                idx_ab[a, b] = ct
+                ndim += 1
+            ct += 1
+    return idx_ab, ndim
--- ccpy/eom_guess/dipcis.py
+++ ccpy/eom_guess/dipcis.py
@@ -2,10 +2,21 @@
 DIP-EOMCCSD(2h) Guess Routine for DIP-EOMCC
 '''
 
+import time
 import numpy as np
 from ccpy.eom_guess.s2matrix import spin_adapt_guess
 
 def run_diagonalization(system, H, multiplicity, roots_per_irrep, nacto, nactu, debug=False, use_symmetry=False):
+
+    nroots_total = 0
+    for key, value in roots_per_irrep.items():
+        nroots_total += value
+
+    noa, nob, nua, nub = H.ab.oovv.shape
+    ndim = noa*nob
+    V = np.zeros((ndim, nroots_total))
+    omega_guess = np.zeros(nroots_total)
+    n_found = 0
 
     # print results of initial guess procedure
     print("   DIP-CIS initial guess routine")
@@ -16,23 +27,39 @@ def run_diagonalization(system, H, multiplicity, roots_per_irrep, nacto, nactu, 
     print("   Active unoccupied alpha = ", min(nactu, system.nunoccupied_alpha))
     print("   Active unoccupied beta = ", min(nactu + (system.multiplicity - 1), system.nunoccupied_beta))
 
-    nroot = 0
-    for irrep, nroot_irrep in roots_per_irrep.items():
-        nroot += nroot_irrep
+    for irrep, nroot in roots_per_irrep.items():
+        if nroot == 0: continue
+        if not use_symmetry: irrep = None
 
-    Hmat = build_2h_hamiltonian(H, system, nacto)
-    S2mat = build_s2matrix_2h(system, nacto)
-    omega, V_act = spin_adapt_guess(S2mat, Hmat, multiplicity, debug=debug)
+        # Build the indexing arrays for the given irrep
+        idx_ab, ndim_irrep = get_index_arrays(nacto, nactu, system, irrep)
+        t1 = time.time()
+        # Compute the active-space 2h Hamiltonian
+        Hmat = build_2h_hamiltonian(H, nacto, idx_ab, system)
+        # Compute the S2 matrix in the same projection subspace
+        S2mat = build_s2matrix_2h(system, nacto, idx_ab)
+        # Project H onto the spin subspace with the specified multiplicity
+        omega, V_act = spin_adapt_guess(S2mat, Hmat, multiplicity, debug=debug)
+        nroot = min(nroot, V_act.shape[1])
+        kout = 0
+        for i in range(len(omega)):
+            if omega[i] == 0.0: continue
+            V[:, n_found] = scatter(V_act[:, i], nacto, system)
+            omega_guess[n_found] = omega[i]
+            n_found += 1
+            kout += 1
+            if kout == nroot:
+                break
 
-    nroot = min(nroot, V_act.shape[1])
-
-    # scatter active-space guess into full space
-    V = np.zeros((system.noccupied_alpha*system.noccupied_beta, nroot))
-    for i in range(nroot):
-        V[:, i] = scatter(V_act[:, i], nacto, system)
-        print("   Eigenvalue of root", i + 1, " = ", np.round(omega[i], 8))
+        elapsed_time = time.time() - t1
+        print("   -----------------------------------")
+        print("   Target symmetry irrep = ", irrep, f"({system.point_group})")
+        print("   Dimension of eigenvalue problem = ", ndim_irrep)
+        print("   Elapsed time = ", np.round(elapsed_time, 2), "seconds")
+        for i in range(n_found - kout, n_found):
+            print("   Eigenvalue of root", i + 1, " = ", np.round(omega_guess[i], 8))
     print("")
-    return omega, V
+    return omega_guess, V
 
 def scatter(V_in, nacto, system):
 
@@ -48,7 +75,7 @@ def scatter(V_in, nacto, system):
             ct += 1
     return V_out
 
-def build_2h_hamiltonian(H, system, nacto):
+def build_2h_hamiltonian(H, nacto, idx_ab, system):
 
     noa = system.noccupied_alpha
     nob = system.noccupied_beta
@@ -63,22 +90,24 @@ def build_2h_hamiltonian(H, system, nacto):
     n2b = nacto_a * nacto_b
 
     Hab = np.zeros((n2b, n2b))
-    ct1 = 0
     for i in range(noa - nacto_a, noa):
         for j in range(nob - nacto_b, nob):
-            ct2 = 0
+            idet = idx_ab[i, j]
+            if idet == 0: continue
+            ind1 = abs(idet) - 1
             for k in range(noa - nacto_a, noa):
                 for l in range(nob - nacto_b, nob):
-                    Hab[ct1, ct2] = (
-                        - H.b.oo[l, j] * (i == k)
-                        - H.a.oo[k, i] * (j == l)
-                        + H.ab.oooo[k, l, i, j]
-                    )
-                    ct2 += 1
-            ct1 += 1
+                    jdet = idx_ab[k, l]
+                    if jdet != 0:
+                        ind2 = abs(jdet - 1)
+                        Hab[ind1, ind2] = (
+                            - H.b.oo[l, j] * (i == k)
+                            - H.a.oo[k, i] * (j == l)
+                            + H.ab.oooo[k, l, i, j]
+                        )
     return Hab
 
-def build_s2matrix_2h(system, nacto):
+def build_s2matrix_2h(system, nacto, idx_ab):
 
     noa = system.noccupied_alpha
     nob = system.noccupied_beta
@@ -93,16 +122,51 @@ def build_s2matrix_2h(system, nacto):
     n2b = nacto_a * nacto_b
 
     Sab = np.zeros((n2b, n2b))
-    ct1 = 0
     for i in range(noa - nacto_a, noa):
         for j in range(nob - nacto_b, nob):
-            ct2 = 0
+            idet = idx_ab[i, j]
+            if idet == 0: continue
+            ind1 = abs(idet) - 1
             for k in range(noa - nacto_a, noa):
                 for l in range(nob - nacto_b, nob):
-                    Sab[ct1, ct2] = (
-                           (i == k) * (j == l)
-                         - (i == l) * (j == k)
-                    )
-                    ct2 += 1
-            ct1 += 1
+                    jdet = idx_ab[k, l]
+                    if jdet != 0:
+                        ind2 = abs(jdet - 1)
+                        Sab[ind1, ind2] = (
+                               (i == k) * (j == l)
+                             - (i == l) * (j == k)
+                        )
     return Sab
+
+def get_index_arrays(nacto, nactu, system, target_irrep):
+
+    noa = system.noccupied_alpha
+    nua = system.nunoccupied_alpha
+    nob = system.noccupied_beta
+    nub = system.nunoccupied_beta
+
+    # set active space parameters
+    nacto_a = min(nacto + (system.multiplicity - 1), noa)
+    nacto_b = min(nacto, nob)
+    # nactu_a = min(nactu, nua)
+    # nactu_b = min(nactu + (system.multiplicity - 1), nub)
+
+    if target_irrep is None:
+        sym1 = lambda i, j: True
+    else:
+        sym = lambda orbital_number: system.point_group_irrep_to_number[system.orbital_symmetries[orbital_number]]
+        ref_sym = system.point_group_irrep_to_number[system.reference_symmetry]
+        target_sym = system.point_group_irrep_to_number[target_irrep]
+
+        sym1 = lambda i, j: sym(i) ^ sym(j) ^ ref_sym == target_sym
+
+    ndim = 0
+    idx_ab = np.zeros((noa, nob), dtype=np.int32)
+    ct = 1
+    for i in range(noa - nacto_a, noa):
+        for j in range(nob - nacto_b, nob):
+            if sym1(i, j):
+                idx_ab[i, j] = ct
+                ndim += 1
+            ct += 1
+    return idx_ab, ndim
